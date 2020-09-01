2a0440f0e7e6e19c59f3f95c3e2c4235d415432c
Edison <edison4354@gmail.com>
2020-09-01 12:43:13 -0700

    Finished

---
+++ functions.py
@@ -0,0 +1,39 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+result1 = area(4,10)
+result2 = area(3,7)
+print(result)
+print(result1)
+print(result2)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+minus = subtract(1,5)
+minus1 = subtract(3,2)
+minus2 = subtract(8,2)
+print(minus)
+print(minus1)
+print(minus2)
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+div = divide(2,4)
+div1 = divide(3,6)
+print(div)
+print(div1)
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+sum1 = add(2,3)
+sum2 = add(5,8)
+print(sum1)
+print(sum2)
